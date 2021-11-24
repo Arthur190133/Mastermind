72b100277ae14eeff2e99524fc3ace5ecac5223e
Arthur190133 <91669888+Arthur190133@users.noreply.github.com>
2021-11-24 18:11:20 +0100

    Ajout de commentaires

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -5,7 +5,7 @@ namespace mastermind
 {
     public class Program
     {
-
+        // Enumeration de toutes les couleurs disponible
         public enum color
         {
             ROUGE,
@@ -16,6 +16,7 @@ namespace mastermind
             ORANGE
         }
 
+        // Structure de la derniere manche
         public struct last_round
         {
             public color[] player_combi;
@@ -31,16 +32,17 @@ namespace mastermind
             color[] secret_combi; // combinaison secrete 
             color[] player_combi = new color [4]; // combinaison du joueur
 
-            bool correct_combi = false;
+            bool correct_combi = false; // la combinaison est correcte
 
-            int max_round = 0;
-            int max_game = 0;
-            int current_round = 0;
-            int current_game = 0;
-            int total_game_win = 0;
-            int combi_white = 0;
-            int combi_red = 0;
+            int max_round = 0; // nombre maximum de manche 
+            int max_game = 0; // nombre maximum de partie
+            int current_round = 0; // manche actuelle
+            int current_game = 0; // partie actuelle 
+            int total_game_win = 0; // nombre total de victoire du joueur
+            int combi_white = 0; // nombre de pion blanc du joueur
+            int combi_red = 0; // nombre de pion rouge du joueur 
 
+            // Afficher le logo MasterMind
             Console.WriteLine(",--.   ,--.                ,--.                ,--.   ,--.,--.           ,--.");
             Console.WriteLine("|   `.'   | ,--,--. ,---.,-'  '-. ,---. ,--.--.|   `.'   |`--',--,--,  ,-|  |");
             Console.WriteLine("|  |'.'|  |' ,-.  |(  .-''-.  .-'| .-. :|  .--'|  |'.'|  |,--.|      \' .-.  |");
@@ -49,66 +51,76 @@ namespace mastermind
             Console.WriteLine(" ");
             Console.WriteLine("------------------------------------------------------------------------------\n\r\n\r\n\r");
 
-            set_rules(ref max_game, ref max_round, ref all_round);
-            rules(max_game,  max_round);
+            set_rules(ref max_game, ref max_round, ref all_round); // personnaliser les règles du jeu
+            rules(max_game,  max_round); // afficher les règles
 
-            random_secret_combi(out secret_combi);
-            Show_secret_cobmbi(secret_combi);
+            random_secret_combi(out secret_combi); // choisir une combinaison aléatoire
+            Show_secret_cobmbi(secret_combi); // Afficher la combinaison secrète
             
-            start_game(ref current_game);
-            start_round(max_round, ref current_round);
+            start_game(ref current_game); // lancer la partie
+            start_round(max_round, ref current_round); // lancer la manche
 
-            Get_player_combi(ref player_combi);
-            Get_player_combi_red(secret_combi, player_combi, ref correct_combi, ref combi_red);
-            Get_player_combi_white(secret_combi, player_combi, ref combi_white);
+            Get_player_combi(ref player_combi); // récuperer la combinaison du joueur
+            Get_player_combi_red(secret_combi, player_combi, ref correct_combi, ref combi_red); // récuperer les pions rouges du joueur
+            Get_player_combi_white(secret_combi, player_combi, ref combi_white); // récuperer les pions blancs du joueur
 
-            end_round(max_round, max_game, secret_combi, ref current_game, ref current_round, ref correct_combi, ref total_game_win, ref combi_red, ref combi_white, all_round, player_combi);
+            end_round(max_round, max_game, secret_combi, ref current_game, ref current_round, ref correct_combi, ref total_game_win, ref combi_red, ref combi_white, all_round, player_combi); // fin de manche 
 
             Console.ReadLine();
         }
 
+        // Lancer la partie
         static void start_game(ref int current_game)
         {
-            current_game = current_game + 1;
+            current_game = current_game + 1; // ajouter 1 au nombre de partie actuelle
 
+            // aficher la partie actuelle 
             Console.WriteLine("----------------------------------------------------------------\n\r");
             Console.WriteLine($"--------------------------Partie(s) : {current_game}-------------------------");
             Console.WriteLine("\n\r----------------------------------------------------------------\n\r\n\r");
 
         }
 
+        // Personnaliser les règles du jeu
         static void set_rules(ref int max_game, ref int max_round, ref last_round[] all_round)
         {
 
-            const int d_max_round = 12;
-            const int d_max_game = 3;
+            const int d_max_round = 12; // nombre par défault de manche
+            const int d_max_game = 3; // nombre par défault de partie
 
-            bool correct = false;
+            bool correct = false; // les régles du jeu sont correctes ( par default = false )
 
-            string custom_settings = "";
+            string custom_settings = "NON"; // Le joueur veut personnaliser les règles ( par défault = false ) 
 
+            // Demander si le joueur veut personnaliser les règles du jeu
             Console.WriteLine("Voulez vous personnaliser les paramètres ?");
             custom_settings = Console.ReadLine();
             custom_settings = custom_settings.ToUpper();
 
+            // Personnaliser les règles si le joueur le veut 
             if (custom_settings == "OUI")
             {
                 while (correct == false)
                 {
                     try
                     {
+                        // Demander le nombre maximum de manches 
                         Console.WriteLine("\n\r\n\rEntrez le nombre maximum de manches pour trouver la combinaison secrete ");
                         Console.WriteLine("________________________________________________________________________\n\r");
                         max_round = Int32.Parse(Console.ReadLine());
+
+                        // Demander le nombre maximum de parties
                         Console.WriteLine("Entrez le nombre maximum de parties à jouer");
                         Console.WriteLine("________________________________________________________________________");
                         max_game = Int32.Parse(Console.ReadLine());
+
                         Console.WriteLine("");
                         correct = true;
                         all_round = new last_round [max_round];
                     }
                     catch
                     {
+                        // Afficher que le joueur s'est trompé en écrivant les règles personnalisées
                         Console.WriteLine("Vous avez entrer une valeur incorrecte ! \n\r\n\r");
                         continue;
                     }
@@ -116,7 +128,7 @@ namespace mastermind
             }
             else
             {
-                
+                // valeur par défault 
                 max_round = d_max_round;
                 max_game = d_max_game;
                 all_round = new last_round[max_round];
@@ -125,6 +137,7 @@ namespace mastermind
             all_round[0].player_combi = new color[4];
         }
 
+        // Afficher les règles
         static void rules(int max_game,  int max_round)
         {
             Console.WriteLine("__________________paramètres de la partie__________________\n\r");
@@ -134,16 +147,18 @@ namespace mastermind
             Console.WriteLine($"   Voici toutes les couleurs disponible   : \n\r                         -ROUGE,\n\r                         -BLEU,\n\r                         -VERT,\n\r                         -JAUNE,\n\r                         -VIOLET,\n\r                         -ORANGE\n\r\n\r");
         }
 
+        // Choisir aléatoirement une combinaison secrete 
         static void random_secret_combi(out color[] secret_combi)
         {
             secret_combi = new color[4];
             Random rnd = new Random();
             for(int i = 0; i <= 3; i++){
-                color valeur  = (color)rnd.Next(0, 6);
-                secret_combi[i] = valeur;
+                color valeur  = (color)rnd.Next(0, 6); // couleur aléatoire
+                secret_combi[i] = valeur; // ajout de la couleur aléatoire à la combinaison secréte 
             }
         }
 
+        // Afficher la combinaison secrete 
         static void Show_secret_cobmbi(color[] secret_combi) 
         {
             for(int i = 0; i <= 3; i++)
@@ -152,12 +167,14 @@ namespace mastermind
             }
         }
 
+        // Récuperer la combinaison du joueur
         static void Get_player_combi(ref color[] player_combi)
         {
             for (int i = 0; i != 4; i++)
             {
                 try
                 {
+                    // Demander la couleur au joueur 
                     Console.WriteLine($"entre la couleur numéro : {i + 1}\n\r ");
                     player_combi[i] = (color)Enum.Parse(typeof(color), Console.ReadLine());
                     Console.WriteLine("------------------------------\n\r");
@@ -165,6 +182,7 @@ namespace mastermind
                 }
                 catch
                 {
+                    // Mauvaise combinaison entrée par le joueur 
                     i--;
                     Console.WriteLine("");
                     Console.WriteLine("Vous n'avez pas entrer une couleur valide \n\r Merci de rentrez une nouvelle couleur \n\r");
@@ -175,22 +193,27 @@ namespace mastermind
 
         }
 
+        // Récuperer les pions rouges du joueur 
         static void Get_player_combi_red(color[] player_combi, color[] secret_combi, ref bool correct_combi, ref int combi_red)
         {
             
             for(int i = 0; i < 4; i++)
             {
+                // Si la couleur du joueur actuelle du joueur est égale à celle de la combinaison secrete 
                 if (player_combi[i] == secret_combi[i])
                 {
-                    combi_red++;
+                    combi_red++; // Ajouter 1 au nombre de pions rouge du joueur 
                     
                 }
             }
+            // Si le joueur à 4 pions rouge
             if (combi_red == 4)
             {
-                correct_combi = true;
+                correct_combi = true; // la combinaison est correcte 
 
             }
+
+            // Afficher le nombre de pion rouge
             Console.WriteLine($"{combi_red} : rouge(s)");
             Console.WriteLine("-------------");
 
@@ -222,10 +245,12 @@ namespace mastermind
             
         }
 
+        // Commencer une manche 
         static void start_round(int max_round, ref int current_round)
         {
-            current_round++;
+            current_round++; // ajouter 1 au nombre actuelle de manche
 
+            // Afficher la manche
             Console.WriteLine($"         Manche : {current_round}         ");
             Console.WriteLine($"         {current_round}/{max_round} essai(s)");
             Console.WriteLine("___________________________________\n\r");
@@ -233,10 +258,11 @@ namespace mastermind
 
         }
 
+        // Fin de manche
         static void end_round(int max_round, int max_game, color[] secret_combi, ref int current_game, ref int current_round, ref bool correct_combi, ref int total_game_win, ref int combi_red, ref int combi_white, last_round[] all_round, color[] player_combi)
         {
-            string rounds_summary = ""; 
-            string restart = "";
+            string rounds_summary = " "; // somaire de toutes les manches
+            string restart = "NON"; // relancer le jeu ( par défault = False ) 
 
             //  ajouter les pions blancs / rouges et que le round est passé dans all_round
             all_round[current_round - 1].player_combi = new color[4];
@@ -258,37 +284,47 @@ namespace mastermind
             Console.WriteLine("--------------------------------------\n\r");
             for (int i = 0; i < all_round.Length; i++)
             {
+                // Si la manche est passée 
                 if (all_round[i].past_round == true)
                 {
+                    // Ajouter le numéro de la manche et le nombre de pion rouge
                     rounds_summary = $"manche : {i + 1}   | { all_round[i].combi_bp} : rouge(s) |  ";
                     for (int o = 0; o <4; o++)
                     {
+                        // Ajouter toutes les couleurs du joueur pendant la manche
                         rounds_summary = rounds_summary + (all_round[i].player_combi[o]) + "   |   " ;
                     }
-
+                    // Ajouter le nombre de pion blanc
                     rounds_summary = rounds_summary + $"{ all_round[i].combi_bv} : blanc(s)";
+
+                    // Afficher toutes les manches précédentes 
                     Console.WriteLine(value: $"{rounds_summary}");
                     Console.WriteLine("-----------------------------------------------------------------------------------------");
                 }
 
             }
 
+            // Si le nombre actuelle de manche est égale au nombre maxium de manche OU que la combinaison du joueur est correcte
             if (current_round == max_round || correct_combi == true)
             {
-                current_round = 0;
+                current_round = 0; // mettre à 0 le nombre actuelle 
 
+                // Si la combinaison est correcte 
                 if (correct_combi == true)
                 {
-                    //the player has found the secret combisaison 
+                    // Le joueur à trouvé la combinaison secrete 
+                    // Afficher que le joueur à trouvé la combinaison secrete 
                     Console.WriteLine("\n\rBravo vous avez réussi à trouver la combinaison secrete !\n\r");
-                    total_game_win++;
-                    correct_combi = false; 
+                    total_game_win++; // ajouter 1 au nombre total de victoire
+                    correct_combi = false; // la combinaion n'est plus correcte 
                     
                 }
                 else
                 {
-                    //Fin de la partie actuelle
+                    // Fin de la partie actuelle
+                    // Afficher que le joueur n'a plus d'essaie et afficher son nombre de manche maximum
                     Console.WriteLine($"\n\r\n\rVous n'avez plus d'essais \n\rVous aviez {max_round} essai(s)");
+                    // Afficher la combinaison secrete 
                     Console.WriteLine("Voici la combinaison secrete ! \n\r\n\r");
                     Show_secret_cobmbi(secret_combi);
                     Console.WriteLine("-------------------------------------------------------------\n\r\n\r");
@@ -302,30 +338,32 @@ namespace mastermind
                 }
                 else
                 {
-                    //fin du jeu
+                    // Fin du jeu
+                    // Afficher le nombre de partie que le joueur à joué et le nombre qu'il a gagné
                     Console.WriteLine($"Vous avez joué {max_game} parties\n\rVous avez gagné {total_game_win} parties\n\r");
                     Console.WriteLine("------------------------------------------------------------------------------");
+                    // Demander au joueur s'il veut rejouer 
                     Console.WriteLine("Voulez vous rejouer ? ");
                     restart = Console.ReadLine();
                     restart = restart.ToUpper();
 
-                    //relancer le jeu
+                    // Si le joueur veut rejouer le jeu
                     if (restart == "OUI")
                     {
-                        //restart
+                        // Relancer le jeu
                         Console.Clear();
                         Main();
                     }
                     else
                     {
-                        //quit
+                        // Quitter le jeu
                         Console.WriteLine("Merci d'avoir joué !");
                         System.Environment.Exit(0);
                     }
                 }
 
             }
-            //start new round
+            // Commencer une nouvelle manche
             start_round(max_round, ref current_round);
             Get_player_combi(ref player_combi);
             Get_player_combi_red(secret_combi, player_combi, ref correct_combi, ref combi_red);
